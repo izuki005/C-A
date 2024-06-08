ffa2092753efd62dc974e5dcafd3e44c9aac2a3f
Matheus Falcão de almeida <135279628+izuki005@users.noreply.github.com>
2024-06-08 11:45:23 -0300

    colocado para retorna ao usuário a sua senha

--- back-end/controllers/autenticacao_controle.js
+++ back-end/controllers/autenticacao_controle.js
@@ -1,35 +1,39 @@
 const crypto = require('crypto');
 
 async function verificarLogin(req, res) {
-  const { email, senha } = req.body;
-  if (!email || !senha) {
-      return res.status(400).json({ mensagem: 'Email ou senha não foram fornecidos na solicitação.' });
+    const { email, senha } = req.body;
+    if (!email || !senha) {
+        return res.status(400).json({ mensagem: 'Email ou senha não foram fornecidos na solicitação.' });
+    }
+  
+    try {
+        const criptografada = crypto.createHash('md5').update(senha).digest('hex');
+  
+        const checkLoginQuery = `
+            SELECT id_cadastro, nome, email, senha 
+            FROM cadastro 
+            WHERE email = @Email AND senha = @Senha
+        `;
+  
+        const result = await global.conn.request()
+            .input('Email', email)
+            .input('Senha', criptografada)
+            .query(checkLoginQuery);
+        
+        if (result.recordset.length > 0) {
+            const user = result.recordset[0];
+            // Retornando a senha original em vez da hash
+            user.senha = senha;
+            return res.status(200).json(user);
+        } else {
+            return res.status(404).json({ mensagem: 'Usuário não encontrado.' });
+        }
+    } catch (err) {
+        return res.status(500).json({ mensagem: 'Erro interno no servidor.' });
+    }
   }
-
-  try {
-      const hashedSenha = crypto.createHash('md5').update(senha).digest('hex');
-
-      const checkLoginQuery = `
-          SELECT id_cadastro, nome, email, senha 
-          FROM cadastro 
-          WHERE email = @Email AND senha = @Senha
-      `;
-
-      const result = await global.conn.request()
-          .input('Email', email)
-          .input('Senha', hashedSenha) // Verifica a senha criptografada
-          .query(checkLoginQuery);
-      
-      if (result.recordset.length > 0) {
-          return res.status(200).json(result.recordset[0]);
-      } else {
-          return res.status(404).json({ mensagem: 'Usuário não encontrado.' });
-      }
-  } catch (err) {
-      return res.status(500).json({ mensagem: 'Erro interno no servidor.' });
-  }
-}
-
-module.exports = {
-  verificarLogin
-};
+  
+  module.exports = {
+    verificarLogin
+  };
+  
