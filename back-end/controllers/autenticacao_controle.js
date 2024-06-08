327a3e417a829e40a41b5e85fdce9e5bc5613503
Matheus Falcão de almeida <135279628+izuki005@users.noreply.github.com>
2024-06-08 11:31:16 -0300

    adcionado todas as criptografias

--- back-end/controllers/autenticacao_controle.js
+++ back-end/controllers/autenticacao_controle.js
@@ -1,19 +1,23 @@
+const crypto = require('crypto');
+
 async function verificarLogin(req, res) {
   const { email, senha } = req.body;
   if (!email || !senha) {
       return res.status(400).json({ mensagem: 'Email ou senha não foram fornecidos na solicitação.' });
   }
 
-  const checkLoginQuery = `
-      SELECT id_cadastro, nome, email, senha 
-      FROM cadastro 
-      WHERE email = @Email AND senha = @Senha
-  `;
-
   try {
+      const hashedSenha = crypto.createHash('md5').update(senha).digest('hex');
+
+      const checkLoginQuery = `
+          SELECT id_cadastro, nome, email, senha 
+          FROM cadastro 
+          WHERE email = @Email AND senha = @Senha
+      `;
+
       const result = await global.conn.request()
           .input('Email', email)
-          .input('Senha', senha)
+          .input('Senha', hashedSenha) // Verifica a senha criptografada
           .query(checkLoginQuery);
       
       if (result.recordset.length > 0) {
@@ -22,7 +26,6 @@ async function verificarLogin(req, res) {
           return res.status(404).json({ mensagem: 'Usuário não encontrado.' });
       }
   } catch (err) {
-      // Não é mais necessário console.error
       return res.status(500).json({ mensagem: 'Erro interno no servidor.' });
   }
 }
